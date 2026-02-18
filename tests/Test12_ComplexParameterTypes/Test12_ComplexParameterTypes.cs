474c6f6b362dafa339a46d2eab999a0313b63ba3
Thomas Zitzler <thomas.zitzler@tietoevry.com>
2026-02-18 17:15:42 +0100

    add test

--- tests/Test12_ComplexParameterTypes/Test12_ComplexParameterTypes.cs
+++ tests/Test12_ComplexParameterTypes/Test12_ComplexParameterTypes.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using TDoubles;
 using TDoubles.Tests.ComprehensiveValidation;
 using System;
@@ -39,6 +40,8 @@ public class Test12_ComplexParameterTypes
         exitCode += ValidationHelper.ValidateThrows(mock, m => _ = m.ArrayMethod(new string[] { "test" }), "TDoubles.TDoublesException", "ArrayMethod");
         exitCode += ValidationHelper.ValidateThrows(mock, m => _ = m.GenericMethod<int>(new List<int> { 1, 2, 3 }), "TDoubles.TDoublesException", "GenericMethod");
         exitCode += ValidationHelper.ValidateMemberExists(mock, m => m.CustomMethod(new CustomClass { Value = "test" }), "invoke CustomMethod(CustomClass)");
+        exitCode += ValidationHelper.ValidateThrows(mock, m => _ = m.OverloadMethod<int>(Enumerable.Empty<int>()), "TDoubles.TDoublesException", "OverloadMethod");
+        exitCode += ValidationHelper.ValidateThrows(mock, m => _ = m.OverloadMethod<int>(new List<int>()), "TDoubles.TDoublesException", "OverloadMethod");
         exitCode += ValidationHelper.ValidateMemberExists(mock, m => _ = m.ToString(), "invoke ToString()");
         exitCode += ValidationHelper.ValidateMemberExists(mock, m => _ = m.GetHashCode(), "invoke GetHashCode()");
         exitCode += ValidationHelper.ValidateMemberExists(mock, m => _ = m.Equals(new object()), "invoke Equals(object)");
@@ -56,6 +59,22 @@ public class Test12_ComplexParameterTypes
         exitCode += ValidationHelper.ValidateCall(mock, m => m.ArrayMethod(new string[] { "test" }), "overridden_array");
         exitCode += ValidationHelper.ValidateCall(mock, m => m.GenericMethod<int>(new List<int> { 1, 2, 3 }), 1);
         exitCode += ValidationHelper.ValidateAction(() => mock.CustomMethod(new CustomClass { Value = "x" }), "call CustomMethod");
+
+        // OverloadMethod overrides: both use Func<object, object> due to method-level generic T
+        mock.MockOverrides.OverloadMethod_IEnumerableT = new Func<object, object>(obj =>
+        {
+            var items = (IEnumerable<int>)obj;
+            return items.Count();
+        });
+        
+        mock.MockOverrides.OverloadMethod_ListT = new Func<object, object>(obj =>
+        {
+            var list = (List<int>)obj;
+            return list.Count * 10;
+        });
+
+        exitCode += ValidationHelper.ValidateCall(mock, m => m.OverloadMethod<int>(new int[] { 1, 2, 3 }), 3);
+        exitCode += ValidationHelper.ValidateCall(mock, m => m.OverloadMethod<int>(new List<int> { 1, 2, 3 }), 30);
 
         mock.MockOverrides.ToString = () => "overridden_toString";
         mock.MockOverrides.GetHashCode = () => 12345;
